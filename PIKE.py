d2ba7a32294771e1139d0baacdc2e4c2ec56141e
ivanastas77 <shkliarov.ia@gmail.com>
2026-01-10 23:42:07 +0300

    Update PIKE.py

--- PIKE.py
+++ PIKE.py
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 ALPHABET = ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
 DEFAULT_KEY = PIKE
 
@@ -92,6 +98,7 @@ def menu()
 if __name__ == __main__
     while menu()
         pass
+
 
 
 
